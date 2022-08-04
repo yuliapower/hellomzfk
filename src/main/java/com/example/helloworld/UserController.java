2c662c295c880b9bafde863df266dbb0fa4cde54
yuliapower <suslova.dzhulia2017@yandex.ru>
2022-08-04 21:00:56 +0300

    mm

---
+++ src/main/java/com/example/helloworld/UserController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("hello")
+    public String getHello(){
+        return "Hello";
+    }
+}
