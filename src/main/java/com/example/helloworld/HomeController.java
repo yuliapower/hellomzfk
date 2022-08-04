85d4e523fde29cabb8a3139d1c906b35c7c8f0ef
yuliapower <suslova.dzhulia2017@yandex.ru>
2022-08-04 21:43:56 +0300

    hello

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -2,12 +2,14 @@ package com.example.helloworld;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    @RequestMapping("/home")
+    @GetMapping("/hello")
     public String home(){
-        return "home";
+        return "Hello World";
     }
 }
