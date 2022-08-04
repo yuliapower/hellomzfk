1ceb11a0cbaf438ed20732bfa157db237cf4bbc3
yuliapower <suslova.dzhulia2017@yandex.ru>
2022-08-04 23:58:51 +0300

    hellodvds

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,7 +22,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
 
 
-	@RequestMapping(value = "/hello")
+	@GetMapping(value = "/hello")
 	public String home(){
 		return "Hello World";
 	}
